64ac331cdf394ba2afc6bce126ff8a637813994d
Felip1205 <andresfelipe.marroquinmartin@gmail.com>
2023-11-14 18:41:46 -0500

    funciona mejor backend

--- src/main/java/com/example/demo/FinalApplication.java
+++ src/main/java/com/example/demo/FinalApplication.java
@@ -2,8 +2,10 @@
 
 	import org.springframework.boot.SpringApplication;
 	import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 	@SpringBootApplication
+	@EntityScan("com.example.demo.model")
 	public class FinalApplication {
 
 		public static void main(String[] args) {
--- src/test/java/com/example/demo/FinalApplicationTests.java
+++ src/test/java/com/example/demo/FinalApplicationTests.java
@@ -3,14 +3,10 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest
 class FinalApplicationTests {
 
     @Test
     void contextLoads() {
-        // You can add meaningful assertions here
-        assertThat(true).isTrue();
     }
 }
